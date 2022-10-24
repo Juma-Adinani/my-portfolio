acd72b4952a8aa786e2cc173047291fb2fec5bcb
Juma-Adinani <jumaadinani19@gmail.com>
2022-10-24 11:20:58 +0300

    remove footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div className="credits">
-      Designed by <a href="https://bootstrapmade.com/">bootstrapmade</a>
+      {/* Designed by <a href="https://bootstrapmade.com/">bootstrapmade</a>
       <br />
-      Developed by <a href="#">Juma Adinani</a>
+      Developed by <a href="#">Juma Adinani</a> */}
     </div>
   );
 }
